6fb55c4428555f1910f7c371c441da3df8140145
Ziad <ziad11.mahmoud@gmail.com>
2026-01-05 07:11:55 +0200

    Add nav bar & background tabs

---
+++ lib/tabs/hadeth/hadeth_tab.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HadethTab extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('HadethTab'));
+  }
+}
---
+++ lib/tabs/quran/quran_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuranTab extends StatefulWidget {
+  const QuranTab({super.key});
+
+  @override
+  State<QuranTab> createState() => _QuranTabState();
+}
+
+class _QuranTabState extends State<QuranTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('QuranTab'));
+  }
+}
