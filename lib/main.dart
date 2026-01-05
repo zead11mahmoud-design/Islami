1e653e70d7f679fc8b70d786c3bc89c166a6c366
Ziad <ziad11.mahmoud@gmail.com>
2026-01-05 07:24:20 +0200

    Implement quran tab UI & logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:islami/app_theme.dart';
 import 'package:islami/home_screen.dart';
 import 'package:islami/intro_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:islami/tabs/quran/quran_service.dart';
+import 'package:islami/tabs/quran/sura_detiails_screen.dart';
 // import 'package:islami/tabs/hadeth/hadeth_details_screen.dart';
-// import 'package:islami/tabs/quran/quran_service.dart';
-// import 'package:islami/tabs/quran/sura_detiails_screen.dart';
 
 Future<void> main() async {
   await WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   final bool isIntroSeen = prefs.getBool('intro_seen') ?? false;
-
-  // QuranService.getMostRecentlySuras();
+  QuranService.getMostRecentlySuras();
   runApp(IslamiApp(isIntroSeen: isIntroSeen));
 }
 
@@ -29,7 +28,7 @@ class IslamiApp extends StatelessWidget {
       routes: {
         IntroScreen.routeName: (_) => IntroScreen(),
         HomeScreen.routeName: (_) => HomeScreen(),
-        // SuraDetiailsScreen.routeName: (_) => SuraDetiailsScreen(),
+        SuraDetiailsScreen.routeName: (_) => SuraDetiailsScreen(),
         // HadethDetiailsScreen.routeName: (_) => HadethDetiailsScreen(),
       },
       initialRoute: isIntroSeen ? HomeScreen.routeName : IntroScreen.routeName,
